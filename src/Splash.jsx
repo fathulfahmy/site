6b3924b1748bfd40ce5e2e8145559bcc1367d349
fathulfahmy <fathulfmy@gmail.com>
2024-02-28 03:51:16 +0800

    feat: replace dvh with svh

--- src/Splash.jsx
+++ src/Splash.jsx
@@ -7,7 +7,7 @@ function Splash() {
         className="pt-[52px] grid max-lg:grid-rows-12 lg:grid-cols-12"
       >
         <div className="border-b-[1px] max-lg:row-span-6 col-span-6">
-          <div className="min-h-[512px] h-[calc(100dvh-52px)] flex flex-col py-[16px] px-[16px] lg:px-[40px]">
+          <div className="min-h-[512px] h-[calc(100svh-52px)] flex flex-col py-[16px] px-[16px] lg:px-[40px]">
             <h1 className="grow text-[12vw] leading-none font-semibold">
               Fathul Fahmy
             </h1>
@@ -17,7 +17,7 @@ function Splash() {
           </div>
         </div>
         <div className="border-b-[1px] lg:border-l-[1px] max-lg:row-span-6 col-span-6 ">
-          <div className="flex flex-col justify-end min-h-[512px] h-[calc(100dvh-52px)] w-full">
+          <div className="flex flex-col justify-end min-h-[512px] h-[calc(100svh-52px)] w-full">
             <img src={potrait} className="object-contain max-h-full" />
           </div>
         </div>
