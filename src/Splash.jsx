9709544af617480302312d71fa96327371e87482
fathulfahmy <fathulfmy@gmail.com>
2024-02-29 03:00:20 +0800

    feat: use fixed height on mobile

--- src/Splash.jsx
+++ src/Splash.jsx
@@ -1,35 +1,25 @@
-import { useWindowSize } from "@uidotdev/usehooks";
+import typography from "./assets/text_long.png";
 import potrait from "./assets/potrait_lg.png";
 
 function Splash() {
-  const screenSize = useWindowSize();
-
   return (
-    <>
-      <div id="splash" className="grid max-lg:grid-rows-12 lg:grid-cols-12">
-        <div className="border-b-[1px] max-lg:row-span-6 col-span-6">
-          <div
-            className="min-h-[512px] flex flex-col pb-[16px] px-[16px] pt-[calc(52px+16px)] lg:px-[40px]"
-            style={{ height: screenSize.height }}
-          >
-            <h1 className="grow text-[12vw] leading-none font-semibold">
-              Fathul Fahmy
-            </h1>
-            <div className="text-xl lg:text-2xl">
-              Student . Developer . Designer .
-            </div>
+    <div id="splash" className="grid lg:grid-cols-12">
+      <div className="border-b-[1px] col-span-6">
+        <div className="flex flex-col min-h-[512px] lg:h-svh px-[16px] pb-[16px] pt-[calc(52px+16px)] lg:px-[40px]">
+          <div className="grow">
+            <img src={typography} className="object-contain" />
           </div>
-        </div>
-        <div className="border-b-[1px] lg:border-l-[1px] max-lg:row-span-6 col-span-6 ">
-          <div
-            className="flex flex-col justify-end min-h-[512px] w-full"
-            style={{ height: screenSize.height }}
-          >
-            <img src={potrait} className="object-contain max-h-full" />
+          <div className="text-xl lg:text-2xl">
+            Student . Developer . Designer .
           </div>
         </div>
       </div>
-    </>
+      <div className="border-b-[1px] lg:border-l-[1px] col-span-6 ">
+        <div className="flex flex-col justify-end min-h-[512px] lg:h-svh">
+          <img src={potrait} className="object-contain max-h-full" />
+        </div>
+      </div>
+    </div>
   );
 }
 
