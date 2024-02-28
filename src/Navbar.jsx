a4174a770f2c2ec532943b5305199149cf78719d
fathulfahmy <fathulfmy@gmail.com>
2024-02-29 01:50:38 +0800

    fix: remove navbar toggle from titlebar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import Hamburger from "./Hamburger.jsx";
+import Close from "./Close.jsx";
 
 function Navbar() {
   const [isOpen, setOpen] = useState(false);
-  function toggleOpen() {
+  function toggleNav() {
     setOpen(!isOpen);
+  }
+  function closeNav() {
+    setOpen(false);
   }
   return (
     <>
@@ -12,31 +16,35 @@ function Navbar() {
         <div className="lg:grid grid-cols-12">
           <div className="col-span-6">
             <div className="border-b-[1px] flex">
-              <a href="#" className="p-[16px] grow hover:bg-neutral-300 " onClick={toggleOpen}>
+              <a
+                href="#"
+                className="p-[16px] grow hover:bg-neutral-300 "
+                onClick={closeNav}
+              >
                 Fathul Fahmy
               </a>
               <button
                 className="p-[16px] border-l-[1px] lg:hidden hover:bg-neutral-300"
-                onClick={toggleOpen}
+                onClick={toggleNav}
               >
-                <Hamburger />
+                {isOpen ? <Close /> : <Hamburger />}
               </button>
             </div>
           </div>
 
           {isOpen && (
             <div className="max-lg:block lg:hidden">
-              <a href="#projects" onClick={toggleOpen}>
+              <a href="#projects" onClick={toggleNav}>
                 <div className="navbar__tabs hover:bg-red-300 focus:bg-red-300">
                   <p className="text-center">Projects</p>
                 </div>
               </a>
-              <a href="#about" onClick={toggleOpen}>
+              <a href="#about" onClick={toggleNav}>
                 <div className="navbar__tabs hover:bg-green-300 focus:bg-green-300">
                   <p className="text-center">About</p>
                 </div>
               </a>
-              <a href="#contact" onClick={toggleOpen}>
+              <a href="#contact" onClick={toggleNav}>
                 <div className="navbar__tabs hover:bg-blue-300 focus:bg-blue-300">
                   <p className="text-center">Contact</p>
                 </div>
