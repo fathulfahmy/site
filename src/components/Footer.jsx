54ab0ea6548adcdbba7c579ea3eaeb7f5c4c0584
fathulfahmy <fathulfmy@gmail.com>
2024-03-02 21:49:53 +0800

    feat: new desktop footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="border-t p-4 pb-16 max-md:text-center md:p-8 md:pb-20">
+    <footer className="border-t p-4 pb-16 max-md:text-center md:p-8 md:pt-20">
       <p>
         fathulfahmy/site{" "}
         <a
